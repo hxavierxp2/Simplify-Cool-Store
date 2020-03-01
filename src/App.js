215aa70f8730e5e4a6345cd507d8314bd74eaa82
HXAVIERXP2 <hxavierxp2@gmail.com>
2020-02-29 22:01:07 -0300

    Preparação Base do Projeto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { store, persistor } from '../src/store/';
+import { Provider } from 'react-redux';
+import { PersistGate} from 'redux-persist/integration/react';
+
+/*PÁGINAS*/
+
+
+function App() {
+  return (
+
+  );
+}
+
+export default App;
